32f1bf8b71d997cb69982154f4a556f3895e8ccc
Hazel <seanobrien0206@gmail.com>
2022-03-06 10:32:40 -0600

    Setup

---
+++ src/main/java/net/firstmod/FirstMod.java
@@ -0,0 +1,13 @@
+package net.firstmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class FirstMod implements ModInitializer{
+
+    @Override
+    public void onInitialize() {
+        
+        
+    }
+    
+}
